71d0352a9c31738e6d275cf3c770752b2fb1b889
LawrenceEzealor <ezealor20@gmail.com>
2023-12-15 17:02:03 +0100

    new commit

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the numbers, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question:"What is the Primary function of Javascript?",
+        answer: "Creating dynamic behaviours",
+        options: [
+            "Creating dynamic behaviours",
+            "Styling web pages",
+            "Sending emails",
+            "Managing databases"
+        ]
+    },
+     {
+        numb: 2,
+        question:"Comment a single line in JavaScript?",
+        answer: "// This is a comment",
+        options: [
+            "// This is a comment",
+            "/* This is a comment */",
+            "# This is a comment",
+            "## This is a comment",
+        ]
+    },
+     {
+        numb: 3,
+        question:"Function is use to convert a string to an integer in JavaScript?",
+        answer: "parseInt()",
+        options: [
+            "parseInteger()",
+            "toInt()",
+            "parseInt()",
+            "stringToInteger()"
+        ]
+    },
+     {
+        numb: 4,
+        question:"What does the typeof operator in JavaScript return for an array?",
+        answer: "object",
+        options: [
+            "array",
+            "object",
+            "string",
+            "type"
+        ]
+    },
+     {
+        numb: 5,
+        question:"Which of the following methods can be used to add an element to the end of an array?",
+        answer: "push()",
+        options: [
+            "push()",
+            "append()",
+            "addToEnd()",
+            "addElement()"
+        ]
+    },
+    {
+        numb: 6,
+        question:"What is the correct way to declare a JavaScript variable?",
+        answer: "var variable = value;",
+        options: [
+            "variable = value;",
+            "var = value;",
+            "value variable;",
+            "var variable = value;"
+        ]
+    },
+    {
+        numb: 7,
+        question:"Purpose of the typeof operator in JavaScript?",
+        answer: "It returns the type of the variable",
+        options: [
+            "It returns the type of the variable",
+            "It creates a new variable",
+            " It compares two variables for equality",
+            "It performs a type conversion."
+        ]
+    },
+    {
+        numb: 8,
+        question:" Purpose of the === operator in JavaScript?",
+        answer: "It checks for strict equality",
+        options: [
+            "It performs addition",
+            "It assigns a value to a variable.",
+            "It checks for strict equality.",
+            "It creates a function."
+        ]
+    },
+    {
+        numb: 9,
+        question:"Function is used to parse a string as JSON in JavaScript?",
+        answer: "JSON.parse()",
+        options: [
+            "parseJSON()",
+            "stringifyJSON()",
+            "JSON.parse()",
+            "JSON.stringify()"
+        ]
+    },
+    {
+        numb: 10,
+        question:"What is the result of 5 > 3 && 2 < 4?",
+        answer: "true",
+        options: [
+            "true",
+            "false",
+            "5",
+            "3"
+        ]
+    },
+]; 
